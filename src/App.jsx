cdec36541a2c48c770aea76c95d107cbc1455cab
AkshatJha21 <akshatj2134@gmail.com>
2024-07-10 23:09:25 +0530

    whileHover, whileTap, MotionConfig, transitions

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import BasicMotion from './components/BasicMotion'
+import Gestures from './components/Gestures'
 
 function App() {
 
   return (
     <div>
-      <BasicMotion />
+      {/* <BasicMotion /> */}
+      <Gestures />
     </div>
   )
 }
